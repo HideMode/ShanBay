3e93fa14a13228daaeaca6fb05450e9a86f59756
Grand Control <cptao@dunoinfo.com>
2016-05-17 10:54:28 +0800

    no message

--- static/javascripts/app.bulid.js
+++ static/javascripts/app.bulid.js
@@ -6,17 +6,17 @@
 	// name: "require",
 	// }],
 	paths: {
-		underscore: "../bower_components/underscore/underscore",
-		jquery: "../bower_components/jquery/dist/jquery",
-		angular: "../bower_components/angular/angular",
-		uiRouter: "../bower_components/angular-ui-router/release/angular-ui-router",
-		uiBootstrap: "../bower_components/angular-bootstrap/ui-bootstrap.min",
-		uiBootstrapTpls: "../bower_components/angular-bootstrap/ui-bootstrap-tpls.min",
-		cropper: "../bower_components/cropper/dist/cropper.min",
-		ngAria: "../bower_components/angular-aria/angular-aria",
-		ngCookies: "../bower_components/angular-cookies/angular-cookies",
-		ngSanitize: "../bower_components/angular-sanitize/angular-sanitize",
-		snackbar: "../bower_components/snackbarjs/dist/snackbar.min"
+		'underscore': "../bower_components/underscore/underscore",
+		'jquery': "../bower_components/jquery/dist/jquery",
+		'angular': "../bower_components/angular/angular",
+		'angular-ui-router': "../bower_components/angular-ui-router/release/angular-ui-router",
+		'ui-bootstrap.min': "../bower_components/angular-bootstrap/ui-bootstrap.min",
+		'ui-bootstrap-tpls.min': "../bower_components/angular-bootstrap/ui-bootstrap-tpls.min",
+		'cropper.min': "../bower_components/cropper/dist/cropper.min",
+		'angular-aria': "../bower_components/angular-aria/angular-aria",
+		'angular-cookies': "../bower_components/angular-cookies/angular-cookies",
+		'angular-sanitize': "../bower_components/angular-sanitize/angular-sanitize",
+		'snackbar.min': "../bower_components/snackbarjs/dist/snackbar.min"
 	},
 	shim: {
 		angular: {
